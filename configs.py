8bbc378da32ea1759578ebbb07fe9fe5fc83f641
Abhishek Kumar <131546223+HanSo-Hee@users.noreply.github.com>
2023-05-28 11:35:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,29 +1,28 @@
 # (c) @PredatorHackerzZ || @TeleRoidGroup
-
 import os
 
 
 class Config(object):
-    	API_ID = int(os.environ.get("API_ID", "0"))
-    	API_HASH = os.environ.get("API_HASH")
-    	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    	GROUP_USERNAME = os.environ.get("GROUP_USERNAME")
-    	CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")
-    	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-    	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-    	DATABASE_URL = os.environ.get("DATABASE_URL")
-    	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-    	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
-    	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-    	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-    	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-    	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-    	AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "30"))
-	ABOUT_BOT_TEXT = f"""
+    API_ID = int(os.environ.get("API_ID", "0"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    GROUP_USERNAME = os.environ.get("GROUP_USERNAME")
+    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+    FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+    BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+    OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+    AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", "30"))
+    ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
-Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
+Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
 
 ╭────[ **🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅**]────⍟
 │
@@ -35,7 +34,7 @@ Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit 
 │
 ├🔹📡 **Hosted On:** [𝐇𝐞𝐫𝐨𝐤𝐮](https://heroku.com)
 │
-├🔸👨‍💻 **Developer:** [@𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫](https://t.me/PredatorHackerzZ) 
+├🔸👨‍💻 **Developer:** [@𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫](https://t.me/OwnYourBotz) 
 │
 ├🔹👥 **Bot Support:** [𝐒𝐮𝐩𝐩𝐨𝐫𝐭](https://t.me/TeleRoid14)
 │
@@ -43,25 +42,21 @@ Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit 
 │
 ╰──────[ 😎 ]───────────⍟
 """
-	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@𝐏𝐫𝐞𝐝𝐚𝐭𝐨𝐫𝐇𝐚𝐜𝐤𝐞𝐫𝐳𝐙](https://t.me/PredatorHackerzZ) 
+    ABOUT_DEV_TEXT = f"""
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@PredatorHackerzZ](https://t.me/PredatorHackerzZ)
+ 
+ I am Super noob Please Support My Hard Work
 
-𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐢𝐬 𝐒𝐮𝐩𝐞𝐫 𝐍𝐨𝐨𝐛. 𝐉𝐮𝐬𝐭 𝐋𝐞𝐚𝐫𝐧𝐢𝐧𝐠 𝐟𝐫𝐨𝐦 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐃𝐨𝐜𝐬. 𝐀𝐧𝐝 𝐒𝐞𝐞𝐤𝐢𝐧𝐠 𝐇𝐞𝐥𝐩 𝐅𝐫𝐨𝐦 𝐏𝐫𝐨 𝐂𝐨𝐝𝐞𝐫𝐬\n**@TheTeleRoid**
-
-𝐈𝐟 𝐘𝐨𝐮 𝐰𝐚𝐧𝐭 𝐭𝐨 𝐃𝐨𝐧𝐚𝐭𝐞 𝐎𝐮𝐫 𝐇𝐚𝐫𝐝 𝐖𝐨𝐫𝐤. 𝐘𝐨𝐮 𝐂𝐚𝐧 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐓𝐡𝐞 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫. 
-
-𝐀𝐥𝐬𝐨 𝐫𝐞𝐦𝐞𝐦𝐛𝐞𝐫 𝐭𝐡𝐚𝐭 𝐝𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 𝐰𝐢𝐥𝐥 𝐃𝐞𝐥𝐞𝐭𝐞 𝐀𝐝𝐮𝐥𝐭 𝐂𝐨𝐧𝐭𝐞𝐧𝐭𝐬 𝐟𝐫𝐨𝐦 𝐃𝐚𝐭𝐚𝐛𝐚𝐬𝐞. 𝐒𝐨 𝐛𝐞𝐭𝐭𝐞𝐫 𝐝𝐨𝐧'𝐭 𝐒𝐭𝐨𝐫𝐞 𝐓𝐡𝐨𝐬𝐞 𝐊𝐢𝐧𝐝 𝐨𝐟 𝐓𝐡𝐢𝐧𝐠𝐬.
-
-[**Donate Me**](https://www.paypal.me/AbhishekKumarIN47) (𝐏𝐚𝐲𝐏𝐚𝐥)
+[Donate Me](https://t.me/DonateXrobot)
 """
-	HOME_TEXT = """
+    HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
-How to Use Bot & it's Benefits??
+How to Use Bot & its Benefits??
 
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+📢 Send me any File, and it will be uploaded in My Database, and you will get the File Link.
 
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+⚠️ Benefits: If you have a Telegram Movie Channel or any Copyright Channel, then it's useful for daily usage. You can send me your file, and I will send a permanent link to you, keeping your channel safe from **Copyright Infringement** issues. I support channels as well. You can check the **About Bot** section.
 
-❌ **PORNOGRAPHY CONTENTS** are strictly prohibited & get Permanent Ban.
+❌ **PORNOGRAPHY CONTENTS** are strictly prohibited and will result in a permanent ban.
 """
